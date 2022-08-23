cb2fa30139f0d13c48f9c9fd3ae9244a23588418
tucker <tuckr@unc.edu>
2022-08-23 19:58:40 -0400

    conditionals lesson

---
+++ lessons/conditionals.py
@@ -0,0 +1,19 @@
+"""An example of conditionals (if-else) statements."""
+
+SECRET: int = 3
+
+print("I'm thinking of a number between 1-5, what is it?")
+guess: int = int(input("What is your guess? "))
+
+if guess == SECRET:
+    print("You guessed correctly!!!")
+    print("Have a wonderful day!!!")
+else:
+    print("Sorry, you guessed incorrectly :(")
+    if guess > SECRET:
+        print("You guessed too high...")
+    else:
+        print("You guessed too low...")
+    print("Try running the program again.")
+
+print("Game over.")
